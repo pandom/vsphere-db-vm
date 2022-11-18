3093e5736b794c6d51251f5c41eb018b136d3717
pandom <pandom@users.noreply.github.com>
2022-11-18 14:46:38 +1100

    Adopting prefer

--- main.tf
+++ main.tf
@@ -23,5 +23,5 @@ module "secrets-database" {
   database_name = "${var.project_id}-postgresql"
   database_server_address =  module.vault[0].virtual_machine_ip
   project_id = var.project_id
-  ssl_mode = "disabled"
+
 }
