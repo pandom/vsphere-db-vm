0ae35f08605e40a82a80196aa399edb026d8443e
pandom <pandom@users.noreply.github.com>
2022-11-18 14:42:27 +1100

    Adopt new SSL mode

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "vault" {
 ## Creates Vault DB secrets engine.
 module "secrets-database" {
   source  = "app.terraform.io/burkey/secrets-database/vault"
-  version = "0.0.2"
+  version = "0.0.3"
   database_name = "${var.project_id}-postgresql"
   database_server_address =  module.vault[0].virtual_machine_ip
   project_id = var.project_id
